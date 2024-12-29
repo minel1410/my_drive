164e3dd82df8f1a5c715b7d97d4e08d701b1a65c
minel1410 <msalihagic@hotmail.com>
2024-12-29 22:34:59 +0100

    Add main file for the api

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,17 @@
 import os
-from typing import Union
+from typing import Union, List
 
 
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from mimetypes import guess_type
+import shutil
+import psutil
 
-from models import Item, DriveResponse, ErrorResponse
+import zipfile
+
+from models import DiskInfo, Item, DriveResponse, ErrorResponse, StatusResponse, FolderRequest
 
 app = FastAPI()
 
@@ -28,16 +32,43 @@ def read_root():
     return {"Hello": "World"}
 
 
-BASE_DIRECTORY = "D:\\"  
+@app.get("/drive", response_model=List[DiskInfo])
+async def get_drive_info():
+    try:
+        partitions = psutil.disk_partitions()
+        disk_info = []
 
+        for partition in partitions:
+            try:
+                usage = psutil.disk_usage(partition.mountpoint)
+                disk_info.append(
+                    DiskInfo(
+                        device=partition.device,
+                        mount_point=partition.mountpoint,
+                        filesystem_type=partition.fstype,
+                        total=usage.total,
+                        used=usage.used,
+                        free=usage.free,
+                        percent=usage.percent,
+                    )
+                )
+            except PermissionError:
+                # Ako nemate pristup nekom disku
+                continue
+
+        if not disk_info:
+            raise HTTPException(status_code=404, detail="No disks found")
+
+        return disk_info
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.get("/drive/{file_path:path}", response_model=Union[DriveResponse, ErrorResponse])
 async def read_or_serve_file(file_path: str = ""):
     try:
-        full_path = (
-            os.path.join(BASE_DIRECTORY, file_path) if file_path else BASE_DIRECTORY
-        )
+        full_path = file_path if file_path else "/"
         if os.path.isdir(full_path):
             items = []
             for item in os.listdir(full_path):
@@ -61,17 +92,16 @@ async def read_or_serve_file(file_path: str = ""):
                             item_size = 0
 
                     # Preskoči elemente sa veličinom 0
-                    if item_size > 0:
-                        items.append(
-                            Item(
-                                name=item,
-                                path=f"/drive/{file_path}/{item}",  # Vratiti rutu za preuzimanje
-                                type=item_type,
-                                size=item_size,
-                                created=os.path.getctime(item_path),
-                                modified=os.path.getmtime(item_path),
-                            )
+                    items.append(
+                        Item(
+                            name=item,
+                            path=f"/drive/{file_path}/{item}",  # Vratiti rutu za preuzimanje
+                            type=item_type,
+                            size=item_size,
+                            created=os.path.getctime(item_path),
+                            modified=os.path.getmtime(item_path),
                         )
+                    )
                 except (PermissionError, FileNotFoundError):
                     # Preskoči elemente za koje nemaš pristup ili ne postoje
                     continue
@@ -81,7 +111,7 @@ async def read_or_serve_file(file_path: str = ""):
         elif os.path.isfile(full_path):
             mime_type, _ = guess_type(full_path)
             if mime_type is None:
-                mime_type = "application/octet-stream"
+                mime_type = "application/octet-stream"  # Default to binary if mime type is unknown
 
             # Lista MIME tipova koji se mogu otvoriti u browseru
             viewable_mime_types = [
@@ -107,7 +137,57 @@ async def read_or_serve_file(file_path: str = ""):
             ):
                 return FileResponse(full_path, media_type=mime_type)
 
-            # Ako nije podržan za prikaz, preuzimanje fajla kao prilog
+            # Ako fajl nije podržan za prikaz u browseru, vrati 415 Unsupported Media Type
+            raise HTTPException(
+                status_code=415, detail="File type cannot be opened in the browser"
+            )
+
+        else:
+            raise HTTPException(status_code=404, detail="Path does not exist")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get(
+    "/download/{file_path:path}", response_model=Union[DriveResponse, ErrorResponse]
+)
+async def download_file(file_path: str = ""):
+    try:
+        full_path = file_path if file_path else "/"
+        # Definišite direktorijum u koji ćete sačuvati ZIP fajl
+        temp_dir = (
+            "/tmp"  # Možete promeniti ovo u bilo koji direktorijum koji je dostupan
+        )
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)  # Kreirajte direktorijum ako ne postoji
+
+        # Ako je direktorijum
+        if os.path.isdir(full_path):
+            # Definisanje imena za ZIP fajl
+            zip_filename = f"{os.path.basename(full_path)}.zip"
+            zip_filepath = os.path.join(temp_dir, zip_filename)
+
+            # Kompresovanje direktorijuma u ZIP fajl
+            with zipfile.ZipFile(zip_filepath, "w", zipfile.ZIP_DEFLATED) as zipf:
+                for root, dirs, files in os.walk(full_path):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        zipf.write(file_path, os.path.relpath(file_path, full_path))
+
+            # Vraćanje ZIP fajla kao FileResponse za preuzimanje
+            return FileResponse(
+                zip_filepath,
+                media_type="application/zip",
+                headers={"Content-Disposition": f"attachment; filename={zip_filename}"},
+            )
+
+        # Ako je fajl, samo ga preuzmi
+        elif os.path.isfile(full_path):
+            mime_type, _ = guess_type(full_path)
+            if mime_type is None:
+                mime_type = "application/octet-stream"  # Default to binary if mime type is unknown
+
             return FileResponse(
                 full_path,
                 media_type=mime_type,
@@ -116,8 +196,140 @@ async def read_or_serve_file(file_path: str = ""):
                 },
             )
 
+        # Ako nije ni fajl ni direktorijum
         else:
             raise HTTPException(status_code=404, detail="Path does not exist")
 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.delete("/delete/{file_path:path}")
+async def delete_file(file_path: str = ""):
+    try:
+        full_path = file_path if file_path else "/"
+
+        # Proverite da li fajl ili direktorijum postoji
+        if os.path.exists(full_path):
+            # Ako je to fajl, brišemo ga
+            if os.path.isfile(full_path):
+                os.remove(full_path)
+                return JSONResponse(
+                    status_code=200, content={"message": "File deleted successfully"}
+                )
+
+            # Ako je to direktorijum, proverite da li je prazan
+            elif os.path.isdir(full_path):
+                # Proverite da li je direktorijum prazan
+                if not os.listdir(full_path):  # Direktorijum je prazan
+                    os.rmdir(full_path)  # Brisanje praznog direktorijuma
+                    return JSONResponse(
+                        status_code=200,
+                        content={"message": "Empty directory deleted successfully"},
+                    )
+                else:
+                    # Ako direktorijum nije prazan, vraćamo grešku
+                    raise HTTPException(
+                        status_code=400,
+                        detail="Cannot delete directory. It is not empty.",
+                    )
+        else:
+            raise HTTPException(status_code=404, detail="File or directory not found")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+
+@app.post("/drive/upload", response_model=StatusResponse)
+async def upload_file_to_folder(file: UploadFile = File(...)):
+    file_path = "D:\SLIKE_MOBITEL\Screenshots"
+    try:
+        print(f"Received file: {file.filename}")
+        print(f"Target folder: {file_path}")
+
+        # Proveri da li folder postoji
+        if not os.path.exists(file_path):
+            print(f"Folder '{file_path}' does not exist.")
+            raise HTTPException(
+                status_code=400, detail=f"Target folder '{file_path}' does not exist."
+            )
+
+        # Putanja gde će fajl biti sačuvan
+        target_file_path = os.path.join(file_path, file.filename)
+
+        # Sačuvaj fajl u folder
+        with open(target_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # Vraćamo oba polja: status i message
+        return StatusResponse(
+            status="success",
+            message=f"File '{file.filename}' successfully uploaded to '{file_path}'.",
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/upload/{folder_path:path}")
+async def upload_file(folder_path: str, file: UploadFile = File(...)):
+    """
+    Ruta za prijem fajla i spašavanje u određeni folder.
+
+    Args:
+        folder_path (str): Putanja foldera u koji će fajl biti sačuvan.
+        file (UploadFile): Fajl koji se prenosi.
+
+    Returns:
+        StatusResponse: Odgovor sa statusom i porukom.
+    """
+    try:
+        print(f"Received file: {file.filename}")
+        print(f"Target folder: {folder_path}")
+
+        # Proveri da li folder postoji
+        if not os.path.exists(folder_path):
+            print(f"Folder '{folder_path}' does not exist.")
+            raise HTTPException(
+                status_code=400, detail=f"Target folder '{folder_path}' does not exist."
+            )
+
+        # Kreiraj potpunu putanju do fajla
+        target_file_path = os.path.join(folder_path, file.filename)
+
+        # Sačuvaj fajl na određenu lokaciju
+        with open(target_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        return StatusResponse(
+            status="success",
+            message=f"File '{file.filename}' successfully uploaded to '{folder_path}'.",
+        )
+
+    except Exception as e:
+        # Obrada greške i vraćanje odgovora
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
+
+@app.post("/create-folder")
+async def create_folder(request: FolderRequest):
+    try:
+        # Kombinovanje putanje i imena foldera
+        full_path = os.path.join(request.file_path, request.folder_name)
+
+        # Provera da li folder već postoji
+        if os.path.exists(full_path):
+            raise HTTPException(
+                status_code=400,
+                detail=f"Folder '{request.folder_name}' already exists at '{request.file_path}'.",
+            )
+
+        # Kreiranje foldera
+        os.makedirs(full_path)
+
+        return {
+            "status": "success",
+            "message": f"Folder '{request.folder_name}' successfully created at '{request.file_path}'.",
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
