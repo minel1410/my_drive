c98ed12ed35364a32bc295a967d770fbfb4da5f6
minel1410 <msalihagic@hotmail.com>
2024-12-29 22:35:12 +0100

    Create models for api

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from typing import List
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Item(BaseModel):
@@ -17,3 +18,23 @@ class DriveResponse(BaseModel):
 class ErrorResponse(BaseModel):
     error: str
     status: str
+
+
+class StatusResponse(BaseModel):
+    status: Optional[str] = None
+    message: str
+
+
+class DiskInfo(BaseModel):
+    device: str
+    mount_point: str
+    filesystem_type: str
+    total: int
+    used: int
+    free: int
+    percent: float
+
+
+class FolderRequest(BaseModel):
+    file_path: str
+    folder_name: str
